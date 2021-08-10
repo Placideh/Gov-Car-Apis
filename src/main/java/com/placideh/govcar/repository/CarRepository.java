0a31fd426ee6166edb80a7df5738d9fa25389ad5
placideh <h.uwizeyeplacide@gmail.com>
2021-08-10 14:42:44 +0200

    added all Api operations

--- src/main/java/com/placideh/govcar/repository/CarRepository.java
+++ src/main/java/com/placideh/govcar/repository/CarRepository.java
@@ -1,9 +1,13 @@
 package com.placideh.govcar.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.placideh.govcar.entity.Car;
 
@@ -11,4 +15,8 @@ import com.placideh.govcar.entity.Car;
 public interface CarRepository extends JpaRepository<Car,Integer> {
 	Car findByPlateNo(String plateNo);
 	List<Car> findAll();
+	@Modifying
+	@Transactional
+	@Query("UPDATE Car SET plateNo=?1,institution=?2,manufacturedDate=?3,cost=?4 WHERE id=?5")
+	void updateCar(String plateNo,String institution,LocalDate manufacturedDate,Long cost ,Integer id);
 }
--- src/main/java/com/placideh/govcar/resource/CarResource.java
+++ src/main/java/com/placideh/govcar/resource/CarResource.java
@@ -1,12 +1,18 @@
 package com.placideh.govcar.resource;
 
+import java.time.LocalDate;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +31,46 @@ public class CarResource {
 		Map<String ,String> map=new HashMap<>();
 		map.put("success","car registered!");
 		return new  ResponseEntity<>(map,HttpStatus.CREATED);
+		
+	}
+	@GetMapping("")
+	public ResponseEntity<List<Car>>getAllRegisteredCar() {
+		List<Car>cars=repository.findAll();
+		return new ResponseEntity<>(cars,HttpStatus.OK);
+	}
+	@PutMapping("/update")
+	public ResponseEntity<Map<String,String>> updateCar(@RequestBody Map<String,Object> car){
+		String plateNo=(String)car.get("plateNo");
+		Car carTobeUpdated=repository.findByPlateNo(plateNo);
+		Map<String,String> map=new HashMap<>();
+		if(carTobeUpdated!=null) {
+			Integer id=carTobeUpdated.getId();
+			String institution=(String)car.get("institution");
+			LocalDate date=LocalDate.parse(car.get("manufacturedDate").toString());
+			Long cost=Long.parseLong(car.get("cost").toString());
+			repository.updateCar(plateNo, institution, date, cost, id);
+			map.put("success","vehicle updated");
+			return new ResponseEntity<>(map,HttpStatus.OK);
+		}else {
+			map.put("failed","car not found");
+			return new ResponseEntity<>(map,HttpStatus.NOT_FOUND);
+		}
+		
+	}
+	
+	@DeleteMapping("/deleteplate/{plateNo}")
+	public ResponseEntity<Map<String,String>> removeCar(@PathVariable("plateNo") String plateNo){
+		Car carTobeUpdated=repository.findByPlateNo(plateNo);
+		Map<String,String> map=new HashMap<>();
+		if(carTobeUpdated!=null) {
+			Integer id=carTobeUpdated.getId();
+			repository.deleteById(id);
+			map.put("success","vehicle removed");
+			return new ResponseEntity<>(map,HttpStatus.OK);
+		}else {
+			map.put("failed","car not found");
+			return new ResponseEntity<>(map,HttpStatus.NOT_FOUND);
+		}
 		
 	}
 	
